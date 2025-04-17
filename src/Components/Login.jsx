a5aca3427faa4e5fde4024082f3ac5c2bbf8dd0b
Vamsi-allam <vamsiallam77@gmail.com>
2025-04-17 18:46:28 +0530

    main

---
+++ src/Components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import'../Components/Login.css'
+
+const Login = () => {
+    return (
+        <div className="verifiedEmailContainer">
+            <button type="submit" className="button">
+                Verify Answer
+            </button>
+        </div>
+    );
+};
+
+export default Login
